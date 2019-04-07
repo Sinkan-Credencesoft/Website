aab20bbd8c8d905e05f01626e6e928f9ab72098b
subhakantrout <samaya.muduli@credencesoft.co.nz>
2019-04-07 14:31:35 +0530

    new website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
-
-
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { WelcomeBannerComponent } from './home/welcome-banner/welcome-banner.component';
+import { AboutusComponent } from './home/aboutus/aboutus.component';
+import { ProductsComponent } from './home/products/products.component';
+import { ServicesComponent } from './home/services/services.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
- 
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    WelcomeBannerComponent,
+    AboutusComponent,
+    ProductsComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAYT8pe61MUbk27eiYi9LnnPhwo031Ye7w'
+    })
   ],
-  providers: [
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
