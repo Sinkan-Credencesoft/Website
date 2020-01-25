b36e1549c63336ca361f37e8b72bc02ecf60f6e7
Md Abu Sayeed Chowdhury Abir <abir.sayeed@credencesoft.com>
2020-01-25 15:58:41 +0600

    booking module

--- src/app/site/booking/booking.module.ts
+++ src/app/site/booking/booking.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingComponent } from './booking.component';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { NewBookingComponent } from './new-booking/new-booking.component';
+import { ChooseRoomComponent } from './choose-room/choose-room.component';
+import { CompleteComponent } from './complete/complete.component';
 const routes: Routes = [
-  { path: '', component: BookingComponent}
+  { path: '', component: BookingComponent},
+  { path: 'booking/choose', component: ChooseRoomComponent},
+  { path: 'booking/booking', component: NewBookingComponent},
+  { path: 'booking/payment', component: CheckoutComponent},
+  { path: 'booking/complete', component: CompleteComponent}
 ];
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
-  declarations: [BookingComponent]
+  declarations: [BookingComponent, CheckoutComponent, NewBookingComponent, ChooseRoomComponent, CompleteComponent]
 })
 export class BookingModule { }
